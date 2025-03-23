0c9853471350dd62e7fa545514fee179f7e60e20
Srinu Neelapala <srinu.neelapala@sailssoftware.com>
2025-03-23 10:52:54 +0530

    initial commit

---
+++ src/main/java/com/recruitcrm/employee_performance_management_system/EmployeePerformanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.recruitcrm.employee_performance_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePerformanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePerformanceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruitcrm/employee_performance_management_system/EmployeePerformanceManagementSystemApplicationTests.java
@@ -0,0 +1,10 @@
+package com.recruitcrm.employee_performance_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePerformanceManagementSystemApplicationTests {
+
+
+}
