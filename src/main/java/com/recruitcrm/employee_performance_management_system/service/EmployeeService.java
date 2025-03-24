f7fc382dae2494ee9d7c45ad3376df7684fed23b
Srinu Neelapala <srinu.neelapala@sailssoftware.com>
2025-03-24 11:43:03 +0530

    logging added

--- src/main/java/com/recruitcrm/employee_performance_management_system/service/EmployeeService.java
+++ src/main/java/com/recruitcrm/employee_performance_management_system/service/EmployeeService.java
@@ -9,6 +9,7 @@ import com.recruitcrm.employee_performance_management_system.model.EmployeeRespo
 import com.recruitcrm.employee_performance_management_system.model.PerformanceReviewResponse;
 import com.recruitcrm.employee_performance_management_system.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
@@ -28,15 +30,19 @@ public class EmployeeService {
     public List<EmployeeResponse> getFilteredEmployee(Double performanceScore, LocalDate reviewDate, List<Long> departmentIds, List<Long> projectIds) {
         List<Employee> employeeList = employeeRepository.findEmployeesByFilters(performanceScore, reviewDate, departmentIds, projectIds);
         if(employeeList.isEmpty()){
+            log.info("No Employee Found for given Params - performanceScore:{},reviewDate:{},departments:{},projects:{}"
+                    ,performanceScore,reviewDate,departmentIds,projectIds);
             throw new ResourceNotFoundException("No Employee Found with given Data");
         }
         return employeeMapper.toEmployeeItems(employeeList);
     }
 
     public EmployeeResponse getEmployeeDetailsById(Long employeeId) {
+
         Employee employee = employeeRepository.findEmployeeWithDetails(employeeId)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee with given Id is not found"));
 
+        log.info("Fetched Employee details successfully for Employee Id:{}",employeeId);
         // Sort and limit last 3 performance reviews
         List<PerformanceReviewResponse> lastThreeReviews = employee.getPerformanceReviews().stream()
                 .sorted(Comparator.comparing(PerformanceReview::getReviewDate).reversed())
